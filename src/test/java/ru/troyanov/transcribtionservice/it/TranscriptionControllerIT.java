d491a307c3df41933074daac2792f5b7279ab2be
dmitriy <d-troyanov05@mail.ru>
2024-11-25 21:10:38 +0500

    add integration tests

--- src/test/java/ru/troyanov/transcribtionservice/it/TranscriptionControllerIT.java
+++ src/test/java/ru/troyanov/transcribtionservice/it/TranscriptionControllerIT.java
@@ -1,6 +1,5 @@
 package ru.troyanov.transcribtionservice.it;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,7 +10,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
