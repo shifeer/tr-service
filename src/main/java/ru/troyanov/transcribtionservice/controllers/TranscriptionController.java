dfb5f7f25f9aa953e099861442391b12f824c0ce
dmitriy <d-troyanov05@mail.ru>
2024-12-22 06:45:31 +0500

    deleted service MultiToSimpleFileService.java

--- src/main/java/ru/troyanov/transcribtionservice/controllers/TranscriptionController.java
+++ src/main/java/ru/troyanov/transcribtionservice/controllers/TranscriptionController.java
@@ -2,7 +2,9 @@ package ru.troyanov.transcribtionservice.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,12 +12,16 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.troyanov.transcribtionservice.model.Status;
 import ru.troyanov.transcribtionservice.model.TaskTranscription;
-import ru.troyanov.transcribtionservice.service.MultiToSimpleFileService;
 import ru.troyanov.transcribtionservice.service.StatusTranscriptionHandlerService;
 import ru.troyanov.transcribtionservice.service.TranscriptionService;
 import ru.troyanov.transcribtionservice.validators.ValidFileFormatOrEmpty;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.UUID;
+
+import static java.nio.file.Files.createTempFile;
+import static java.nio.file.Files.write;
 
 @Tag(name = "TranscriptionController")
 @Slf4j
@@ -27,14 +33,15 @@ public class TranscriptionController {
 
     private final TranscriptionService transcriptionService;
     private final StatusTranscriptionHandlerService statusTranscriptionHandlerService;
-    private final MultiToSimpleFileService multiToSimpleFileService;
+    @Value("${path.to.dir}")
+    private Path pathToDir;
 
     @PostMapping
     public ResponseEntity<TaskTranscription> doTranscription(@ValidFileFormatOrEmpty @RequestParam("file") MultipartFile multipartFile,
                                                              @RequestParam("language") String lang) {
 
         String taskId = UUID.randomUUID().toString();
-        transcriptionService.doTranscribe(multiToSimpleFileService.multiToFile(multipartFile), taskId);
+        transcriptionService.doTranscribe(multiToFile(multipartFile), taskId);
 
         return statusTranscriptionHandlerService.getResponse(Status.PROCESSING, taskId);
     }
@@ -48,5 +55,15 @@ public class TranscriptionController {
         }
 
         return statusTranscriptionHandlerService.getResponse(taskId);
+    }
+
+    @SneakyThrows
+    private File multiToFile(MultipartFile multipartFile) {
+
+        byte[] audioBytes = multipartFile.getBytes();
+        Path file = createTempFile(pathToDir, multipartFile.getOriginalFilename(), "");
+        write(file, audioBytes);
+
+        return file.toFile();
     }
 }
