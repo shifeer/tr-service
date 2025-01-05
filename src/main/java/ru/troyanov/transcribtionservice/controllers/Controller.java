a301dc1fcf5b9c5cdcd1c8d264363a1a3bff1721
dmitriy <d-troyanov05@mail.ru>
2025-01-05 17:57:27 +0500

    refact

--- src/main/java/ru/troyanov/transcribtionservice/controllers/Controller.java
+++ src/main/java/ru/troyanov/transcribtionservice/controllers/Controller.java
@@ -1,18 +1,16 @@
 package ru.troyanov.transcribtionservice.controllers;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-    @GetMapping("/main")
+    @GetMapping
     public String index() {
         return "main";
     }
     @GetMapping("/transcription")
     public String loading(@RequestParam(name = "taskId", required = false) String taskId) {
-
         return "transcription";
     }
 }
